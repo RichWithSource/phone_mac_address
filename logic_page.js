c4b56e718ad8f28c8ba1343db9a6b2007b43a152
RichWithSource <jamesrc.debillot@gmail.com>
2026-02-27 14:06:03 +0200

    uuid_mix

--- logic_page.js
+++ logic_page.js
@@ -2,12 +2,13 @@
 //window.alert(uuid)
 
 function button_click(name) {
+    const uuid = crypto.randomUUID()
     if(name.value == "Name"){
         name.value = "Error please enter your name"
         window.alert(name.value)
     }
     else {
-        window.alert(name.value);
+        window.alert(name.value + " " + uuid);
     }
     
 }
