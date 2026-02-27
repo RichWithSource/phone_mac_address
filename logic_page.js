c83221b9406d0eef364d6dfb8a66bc68bc04d964
RichWithSource <jamesrc.debillot@gmail.com>
2026-02-27 09:08:21 +0200

    changes

---
+++ logic_page.js
@@ -0,0 +1,15 @@
+function button_click() {
+    const name = document.getElementById("txtbox");
+    if(name == " " || name == null){
+        window.alert('Error please enter your name')
+    }
+    else {
+        button_clicked(name);
+    }
+    
+}
+
+function button_clicked(a) {
+    window.alert(a);
+}
+
