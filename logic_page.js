4eac29c49cc473dfd876b1ac2d3fb40a49eb8f98
RichWithSource <jamesrc.debillot@gmail.com>
2026-02-27 13:25:00 +0200

    text

--- logic_page.js
+++ logic_page.js
@@ -1,10 +1,10 @@
 function button_click() {
-    const name = document.getElementById("txtbox").textContent();
-    if(name == " " || name == null){
+    const name = document.getElementById("txtbox");
+    if(name.textContent == " " || name.textContent == null){
         window.alert('Error please enter your name')
     }
     else {
-        button_clicked(name);
+        button_clicked(name.textContent);
     }
     
 }
