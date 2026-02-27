5366cb0680c51cd3a2a5d6f7a46dd9af442d017c
RichWithSource <jamesrc.debillot@gmail.com>
2026-02-27 14:45:21 +0200

    change

--- logic_page.js
+++ logic_page.js
@@ -1,29 +1,6 @@
-import { Octokit } from "octkit";
+
 //const uuid = crypto.randomUUID()
 //window.alert(uuid)
-const octokit = new Octokit({auth: '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'})
-
-async function updateGitHubFile(owner, repo, path, message, content) {
-    let sha;
-    try {
-        const {data: fileData } = await octokit.rest.repos.getContent({owner, repo, path});
-        sha = fileData.sha;
-    }
-    catch (error) {
-        console.log("File not found, creating a new one.")
-    }
-
-    await octokit.rest.repos.createOrUpdateFileContents({
-        owner,
-        repo,
-        path,
-        message,
-        content: btoa(content),
-        sha,
-    });
-    console.log(`File ${path} updated successfully!`);
-}
-
 function button_click(name) {
     const uuid = crypto.randomUUID()
     if(name.value == "Name"){
@@ -32,9 +9,7 @@ function button_click(name) {
     }
 
     else {
-        window.alert(name.value + " " + uuid);
-        const log_content = name.value + "--" + uuid
-        updateGitHubFile('RichWithSource','phone_mac_address','RichWithSource/phone_mac_address/list_name.txt','Text Update',log_content)
+        window.alert(name.value + " -- " + uuid);
     }
     
 }
